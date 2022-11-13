dd3b5850a9e87c5209bc7172cf27efefe8169570
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2022-11-13 13:59:32 +0800

    organized code for readability??

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -10,25 +10,37 @@ import com.ctre.phoenix.sensors.WPI_CANCoder;
 import frc.robot.Constants;
 
 public class SwerveModule{
-    private WPI_TalonFX mRotor, mThrottle;
-    private WPI_CANCoder mRotorEncoder;
-
-    public SwerveModule(int rotorID, int throttleID, int CANCoderID, boolean throttleReversed, double rotorOffsetAngle){
+    private WPI_TalonFX mRotor, mThrottle; //the rotor being the motor that rotates the entire wheel module, and throttle being the motor which turns the wheel
+    private WPI_CANCoder mRotorEncoder; //absolute encoder that tracks the absolute angle of the wheel module
+    /**
+     * 
+     * @param rotorID CAN ID of the rotor motor
+     * @param throttleID CAN ID of the throttle motor
+     * @param CANCoderID CAN ID of the CANCoder 
+     * @param throttleReversed checks if the throttle motor is reversed or not
+     * @param rotorOffsetAngle
+     */
+    public SwerveModule(int rotorID, int throttleID, int CANCoderID, boolean throttleReversed, double rotorOffsetAngle)
+    {
         mRotor = new WPI_TalonFX(rotorID);
         mThrottle = new WPI_TalonFX(throttleID);
-        mThrottle.setInverted(throttleReversed);
         mRotorEncoder = new WPI_CANCoder(CANCoderID);
+        mThrottle.setInverted(throttleReversed);
+
         mRotor.configFactoryDefault();
         mThrottle.configFactoryDefault();
         mRotorEncoder.configFactoryDefault();
+
         mRotorEncoder.configAbsoluteSensorRange(AbsoluteSensorRange.Signed_PlusMinus180);	
         mRotorEncoder.configSensorDirection(false);
         mRotorEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
-        mRotor.setInverted(TalonFXInvertType.Clockwise); 
+
         mRotor.configVoltageCompSaturation(Constants.kVoltageCompensation);
-        mThrottle.configVoltageCompSaturation(Constants.kVoltageCompensation);
         mRotor.enableVoltageCompensation(true);
+        mThrottle.configVoltageCompSaturation(Constants.kVoltageCompensation);
         mThrottle.enableVoltageCompensation(true);
+
+        mRotor.setInverted(TalonFXInvertType.Clockwise); 
         mRotor.setNeutralMode(NeutralMode.Brake);
         mThrottle.setNeutralMode(NeutralMode.Brake);
     }
